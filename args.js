f4b5eaea9dd4a0fadf597e5bbe7899486abe6722
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-02 15:07:49 -0600

    completed bonus challenge

--- args.js
+++ args.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+//get the arguments you need without the first two default arguments from process.argv
+//use deconstruction
 let [,,...args] = process.argv;
-let summed = 0;
+//use .map to convert the arguements into numbesr
 let numbersArgs = args.map(parseFloat);
-numbersArgs.forEach((value)=>summed+= value);
-var reduced = numbersArgs.reduce((a, b)=> a+b);
-
+console.log("args", args)
+//if args isn't an array with a length of 0, assign reduce to the sum of it's parts
+//otherwise make reduced = to zero
+var reduced = (args.length === 0) ? 0 : numbersArgs.reduce((a, b)=> a+b);
 console.log(`the sum is ${reduced}.`)
